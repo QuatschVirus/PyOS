48a089e3f21f52d7e2b9ab136db95e257a15c234
QuatschVirus <quatschvirus@gmail.com>
2021-07-25 09:17:35 +0200

    Refactored Installation mechanism

--- download/Installer/PyOS-1.0.0-Lite.bat
+++ download/Installer/PyOS-1.0.0-Lite.bat
@@ -1,10 +1,12 @@
+@echo off
 curl https://eternallybored.org/misc/wget/1.21.1/64/wget.exe -o wget.exe
 wget https://github.com/QuatschVirus/PyOS/raw/main/download/Archives/PyOS-1.0.0-Lite.zip
 tar -xvzf PyOS-1.0.0-Lite.zip
-del libs/placeholder
+mkdir libs
+mkdir programs
 echo Done!
-PAUSE
 del PyOS-1.0.0-Lite.zip
 del .wget-hsts
 del wget.exe
 del PyOS-1.0.0-Lite.bat
+PAUSE
